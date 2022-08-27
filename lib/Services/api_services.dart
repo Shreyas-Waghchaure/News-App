7fea953474764a391d4e8c10cb8081743b655dec
Shreyas-Waghchaure <shreyasforlogin@gmail.com>
2022-08-27 12:04:10 +0530

    api updated

--- lib/Services/api_services.dart
+++ lib/Services/api_services.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:news_app/Model/article_model.dart';
 
   Future <List<NewsApiModel>> getNews() async{
-    Uri uri = Uri.parse("https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=66b8f4acbe114d0d8e64e057b378d273");
+    Uri uri = Uri.parse("https://newsapi.org/v2/everything?domains=techcrunch.com&apiKey=66b8f4acbe114d0d8e64e057b378d273");
     final response = await http.get(uri);
     if(response.statusCode == 200 || response.statusCode == 201){
       print(response.body);
